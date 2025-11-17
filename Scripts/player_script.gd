a3f471dbd76248d6ab6597e5ffce07cacb684bd8
UNIV-LYON1\p1915302 <bastien.chauvet@etu.univ-lyon1.fr>
2025-11-17 17:44:31 +0100

    deplacements et saut et inclinaison camera

--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -3,19 +3,33 @@ extends CharacterBody3D
 
 var health: int = 10
 #var weapon: Weapon
-var movement_speed: float = 10.
+var movement_speed: float = 150.
 var movement_inputs: Vector3
+var max_jump: int = 1
+var jump_count: int = 0
 
 func _physics_process(delta: float) -> void:
-	read_move_inputs()
-	movement_inputs *= movement_speed * delta
-	if movement_inputs != Vector3.ZERO:
-		velocity = movement_inputs * movement_speed
-		move_and_slide()
+	
+	move(delta)
+	jump(delta)
+	move_and_slide()
+	
 	return
 
-func read_move_inputs():
-	movement_inputs.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	movement_inputs.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	movement_inputs = movement_inputs.normalized()
+func move(delta):
+	movement_inputs.x = Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+	movement_inputs.z = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	movement_inputs = movement_inputs.normalized() * delta
+	velocity.x = movement_inputs.x * movement_speed
+	velocity.z = movement_inputs.z * movement_speed
+	return
+
+func jump(delta):
+	if (!is_on_floor()):
+		velocity.y += get_gravity().y * delta
+	else:
+		jump_count = 0
+	if(jump_count < max_jump and Input.is_action_just_pressed("jump")):
+		velocity.y = 5
+		jump_count += 1
 	return
--- game_manager.gd
+++
@@ -1,3 +0,0 @@
-class_name GameManager
-
-extends Node3D
